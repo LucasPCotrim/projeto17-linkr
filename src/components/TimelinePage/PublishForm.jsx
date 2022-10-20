bc11165fdaa4c9fac791a47299c09f9a21c709c7
Lucas-Melo0 <lucassss8v@gmail.com>
2022-10-20 06:58:26 -0300

    feat: Improve Publish form responsiveness

--- src/components/TimelinePage/PublishForm.jsx
+++ src/components/TimelinePage/PublishForm.jsx
@@ -1,9 +1,9 @@
-import styled from 'styled-components';
-import logo from '../../assets/yoda.jpeg';
+import styled from "styled-components";
+import logo from "../../assets/yoda.jpeg";
 
 const PublishForm = ({ handleForm, status }) => {
-  const isLoading = status === 'loading';
-  const isError = status === 'error';
+  const isLoading = status === "loading";
+  const isError = status === "error";
 
   return (
     <Container>
@@ -12,11 +12,20 @@ const PublishForm = ({ handleForm, status }) => {
         <h3>What are you going to share today?</h3>
       </ContainerHeader>
       <Form onSubmit={handleForm}>
-        <input id='url' placeholder='http://...' required disabled={isLoading} />
-        <input id='content' placeholder='Awesome article about #javascript' disabled={isLoading} />
+        <input
+          id="url"
+          placeholder="http://..."
+          required
+          disabled={isLoading}
+        />
+        <input
+          id="content"
+          placeholder="Awesome article about #javascript"
+          disabled={isLoading}
+        />
         <ButtonContainer>
-          <Button disabled={isLoading} type='submit'>
-            {isLoading ? 'Publishing...' : 'Publish'}
+          <Button disabled={isLoading} type="submit">
+            {isLoading ? "Publishing..." : "Publish"}
           </Button>
         </ButtonContainer>
       </Form>
@@ -43,27 +52,38 @@ const ContainerHeader = styled.div`
   display: flex;
   column-gap: 10px;
   align-items: center;
+
   img {
     border-radius: 50%;
     width: 50px;
     height: 50px;
   }
   h3 {
-    font-family: 'Lato';
+    width: 100%;
+    font-family: "Lato";
     font-style: normal;
     font-weight: 300;
     font-size: 20px;
     line-height: 24px;
     color: #707070;
   }
+  @media screen and (max-width: 614px) {
+    h3 {
+      text-align: center;
+    }
+    img {
+      display: none;
+    }
+  }
 `;
 
 const Form = styled.form`
   display: flex;
-  margin-left: 60px;
+  padding-left: 45px;
   width: 90%;
   flex-direction: column;
   row-gap: 8px;
+
   input {
     background: #efefef;
     border-radius: 5px;
