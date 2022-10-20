877a5bc2ea1c33fcaf36f8b0f50456c4b17e4dbc
Lucas-Melo0 <lucassss8v@gmail.com>
2022-10-20 08:55:12 -0300

    feat: Add fetching of posts upon publishing a post

--- src/components/TimelinePage/TimelinePage.jsx
+++ src/components/TimelinePage/TimelinePage.jsx
@@ -1,24 +1,24 @@
-import { useState } from 'react';
-import { publishPost } from '../../services/LinkrAPI';
-import { PublishForm } from './PublishForm';
-import { PostsContainer } from './PostsContainer';
-import styled from 'styled-components';
+import { useState } from "react";
+import { publishPost } from "../../services/LinkrAPI";
+import { PublishForm } from "./PublishForm";
+import { PostsContainer } from "./PostsContainer";
+import styled from "styled-components";
 
 const TimelinePage = () => {
-  const [status, setStatus] = useState('idle');
+  const [status, setStatus] = useState("idle");
   const formHandler = (e) => {
     e.preventDefault();
     const { url, content } = e.target.elements;
-    setStatus('loading');
+    setStatus("loading");
     // Token hardcoded
     publishPost({ url: url.value, content: content.value }, 25).then(
       () => {
-        setStatus('sucess');
+        setStatus("sucess");
         url.value = null;
         content.value = null;
       },
       (error) => {
-        setStatus('error');
+        setStatus("error");
         console.log(error);
       }
     );
@@ -29,7 +29,7 @@ const TimelinePage = () => {
     <Wrapper>
       <header>timeline</header>
       <PublishForm status={status} handleForm={formHandler} />
-      <PostsContainer />
+      <PostsContainer status={status} />
     </Wrapper>
   );
 };
@@ -42,7 +42,7 @@ const Wrapper = styled.div`
   margin-bottom: 100px;
   width: min(100%, 614px);
   header {
-    font-family: 'Oswald';
+    font-family: "Oswald";
     font-style: normal;
     font-weight: 700;
     font-size: 43px;
