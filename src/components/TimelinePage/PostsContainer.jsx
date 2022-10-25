ce19f451366fc1b772010188bcfd08c2bfc721f8
LucasPCotrim <lucaspcotrim@gmail.com>
2022-10-25 16:28:05 -0300

    feat: Implemented dynamic loading of new posts with useInterval hook every 15s

--- src/components/TimelinePage/PostsContainer.jsx
+++ src/components/TimelinePage/PostsContainer.jsx
@@ -1,13 +1,61 @@
-import styled from "styled-components";
-import Post from "./Post";
-import { useEffect, useState } from "react";
-import { getPosts, getPageUser } from "../../services/LinkrAPI";
-import Loading from "../../commons/Loading";
+import styled from 'styled-components';
+import Post from './Post';
+import { useEffect, useState } from 'react';
+import { getPosts, getPageUser } from '../../services/LinkrAPI';
+import Loading from '../../commons/Loading';
+import useInterval from 'use-interval';
+import { BiRefresh } from 'react-icons/bi';
+
+const TIMELINE_REFRESH_INTERVAL = 15000;
+
+const getNumberNewPosts = (posts, newPosts) => {
+  const newPostsIndexes = newPosts.map((post) => post.id);
+  const postsIndexes = posts.map((post) => post.id);
+  return newPostsIndexes.reduce(
+    (prev, curr) => (postsIndexes.indexOf(curr) == -1 ? prev + 1 : prev),
+    0
+  );
+};
+
+function LoadNewPostsButton({ numberNewPosts, setPosts, newLoadedPosts }) {
+  const handleRefresh = () => {
+    setPosts([...newLoadedPosts]);
+  };
+
+  return (
+    <>
+      {numberNewPosts > 0 ? (
+        <NewPostsButtonStyle onClick={() => handleRefresh()}>
+          <h2>{`${numberNewPosts} new ${numberNewPosts > 1 ? 'posts' : 'post'}, load more!`}</h2>
+          <BiRefresh className='icon' />
+        </NewPostsButtonStyle>
+      ) : (
+        <></>
+      )}
+    </>
+  );
+}
 
 function PostsContainer({ status, setStatus, userId = 0, setPageName }) {
   const [posts, setPosts] = useState([]);
+  const [newLoadedPosts, setNewLoadedPosts] = useState([]);
   const [failedToLoadPosts, setFailedToLoadPosts] = useState(false);
   const [loading, setLoading] = useState(false);
+
+  useInterval(() => {
+    if (userId === 0) {
+      const promise = getPosts();
+      promise
+        .then((res) => {
+          setNewLoadedPosts(res.data);
+        })
+        .catch((res) => {
+          console.log(res);
+          setNewLoadedPosts([]);
+          setFailedToLoadPosts(true);
+        });
+    }
+  }, TIMELINE_REFRESH_INTERVAL);
 
   useEffect(() => {
     setLoading(true);
@@ -31,9 +79,8 @@ function PostsContainer({ status, setStatus, userId = 0, setPageName }) {
     return (
       <>
         <Wrapper>
-          <WarningMessage color={"#853232"}>
-            An error occured while trying to fetch the posts, please refresh the
-            page
+          <WarningMessage color={'#853232'}>
+            An error occured while trying to fetch the posts, please refresh the page
           </WarningMessage>
         </Wrapper>
       </>
@@ -43,39 +90,48 @@ function PostsContainer({ status, setStatus, userId = 0, setPageName }) {
     return (
       <>
         <Wrapper>
-          <WarningMessage color={"white"}>Loading</WarningMessage>
-          <Loading color={"white"} />
+          <WarningMessage color={'white'}>Loading</WarningMessage>
+          <Loading color={'white'} />
+        </Wrapper>
+      </>
+    );
+  }
+  if (posts.length === 0) {
+    return (
+      <>
+        <Wrapper>
+          <WarningMessage color={'white'}>There are no posts yet</WarningMessage>
         </Wrapper>
       </>
     );
   }
 
+  const numberNewPosts = getNumberNewPosts(posts, newLoadedPosts);
+
   return (
     <>
       <Wrapper>
-        {posts.length === 0 ? (
-          <WarningMessage color={"white"}>
-            There are no posts yet
-          </WarningMessage>
-        ) : (
-          posts.map((post, index) => {
-            return (
-              <Post
-                key={index}
-                user={post.user}
-                id={post.id}
-                postUrl={post.url}
-                postDescriptionText={post.content}
-                urlMetadata={post.metadata}
-                usersWhoLiked={post.usersWhoLiked}
-                status={status}
-                setStatus={setStatus}
-                hashtagsList={post.hashtagsList}
-              />
-            );
-          })
-        )}
-        {}
+        <LoadNewPostsButton
+          numberNewPosts={numberNewPosts}
+          setPosts={setPosts}
+          newLoadedPosts={newLoadedPosts}
+        />
+        {posts.map((post, index) => {
+          return (
+            <Post
+              key={index}
+              user={post.user}
+              id={post.id}
+              postUrl={post.url}
+              postDescriptionText={post.content}
+              urlMetadata={post.metadata}
+              usersWhoLiked={post.usersWhoLiked}
+              status={status}
+              setStatus={setStatus}
+              hashtagsList={post.hashtagsList}
+            />
+          );
+        })}
       </Wrapper>
     </>
   );
@@ -101,12 +157,37 @@ const WarningMessage = styled.div`
   justify-content: center;
   align-items: center;
   margin-top: 20px;
-  font-family: "Oswald";
+  font-family: 'Oswald';
   font-style: normal;
   font-weight: 500;
   font-size: 24px;
   line-height: 40px;
-  color: ${(props) => props.color || "ffffff"};
+  color: ${(props) => props.color || 'ffffff'};
+`;
+
+const NewPostsButtonStyle = styled.div`
+  width: 100%;
+  height: 61px;
+  background-color: #1877f2;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 16px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  h2 {
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 19px;
+    color: #ffffff;
+    margin-right: 10px;
+  }
+  .icon {
+    font-size: 22px;
+    color: #ffffff;
+  }
 `;
 
 export { PostsContainer };
